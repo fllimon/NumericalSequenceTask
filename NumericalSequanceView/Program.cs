20b129cc776f5fc6c0cda3b2cc5771c46e359363
Artur Sharaenko <49069796+fllimon@users.noreply.github.com>
2021-03-31 15:59:48 +0300

    Add project files.

---
+++ NumericalSequanceView/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NumericalSequenceLib;
+
+namespace NumericalSequanceView
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long numberSequence = long.Parse(args[0]);
+
+            NumericalSequence sequence = new NumericalSequence(numberSequence);
+
+            Console.WriteLine($"Заданное число: {sequence.NumberSequence}");
+            Console.WriteLine();
+
+            foreach (long item in sequence.GetSequence())
+            {
+                Console.Write($"{item}, ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NumericalSequence/NumericalSequence.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalSequenceLib
+{
+    public class NumericalSequence : IEnumerable<long>
+    {
+        #region ======------ PRIVATE DATA -----======
+
+        private long _numberSequence = -1;
+
+        #endregion
+
+        #region ======------ CTOR ------=======
+
+        public NumericalSequence(long numberSequence)
+        {
+            _numberSequence = numberSequence;
+        }
+
+        #endregion
+
+        #region ======----- PROPERTIES ------=====
+
+        public long NumberSequence
+        {
+            get
+            {
+                return _numberSequence;
+            }
+
+            private set
+            {
+                _numberSequence = value;
+            }
+        }
+
+        #endregion
+
+        public IEnumerable<long> GetSequence()
+        {
+            return this;
+        }
+
+        public IEnumerator<long> GetEnumerator()
+        {
+            return new NumericalIterator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new NumericalIterator(this);
+        }
+
+        private struct NumericalIterator : IEnumerator<long>
+        {
+            private long _currentValue;
+            private readonly NumericalSequence _sequence;
+            private bool _isPosition;
+
+            public NumericalIterator(NumericalSequence sequence)
+            {
+                _sequence = sequence;
+                _currentValue = 2;
+                _isPosition = false;
+            }
+
+            long IEnumerator<long>.Current => _currentValue;
+
+            public object Current => _currentValue;
+
+            public bool MoveNext()
+            {
+                if (_isPosition)
+                {
+                    _currentValue *= _currentValue;
+                }
+                _isPosition = true;
+
+                return _currentValue < _sequence._numberSequence;
+            }
+
+            public void Reset()
+            {
+                _currentValue = 0;
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
